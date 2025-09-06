b39eee8c25381eb8cb160928fdafe118d793371d
Aymane <aymane@cashflowpositifi.fr>
2025-09-06 12:03:56 +0200

    Add routes & chirp validation

---
+++ json_response.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func jsonError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	jsonResponse(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func jsonResponse(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
 func main() {
-	fmt.Printf("OkiDoki.\n")
+	const port = "8080"
+	mux := http.NewServeMux()
+
+	cfg := apiConfig{}
+
+	fileHandler := http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir("."))))
+
+	mux.Handle("/app/", fileHandler)
+
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetMetricsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Listening on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
